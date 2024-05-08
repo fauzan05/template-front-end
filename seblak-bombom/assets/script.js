5ab48719360026d618596291f99cf71cd41898de
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2024-05-08 23:06:21 +0700

    menambahkan modal add to cart

--- seblak-bombom/assets/script.js
+++ seblak-bombom/assets/script.js
@@ -1,5 +1,6 @@
 const orderNowButton = document.getElementById("orderNowButton");
 const listAllMenu = document.getElementById("listAllMenu");
+
 if (orderNowButton) {
     orderNowButton.addEventListener("click", () => {
         location.href = "#favourite-order";
@@ -10,3 +11,25 @@ if (listAllMenu) {
         location.href = "#all-menu";
     })
 }
+
+// Button Plus & Minus Count Quantity
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const quantity = document.getElementById("quantity");
+
+if (plusButton && minusButton && quantity) {
+    plusButton.addEventListener("click", () => {
+        quantity.value = parseInt(quantity.value, 10);
+        quantity.value++;
+    })
+    minusButton.addEventListener("click", () => {
+        quantity.value = parseInt(quantity.value, 10);
+        if (quantity.value == 0) {
+            quantity.value = 0;
+        } else  {
+            quantity.value--;
+        }
+    })
+}
+
+// Tambahkan Ke keranjang
